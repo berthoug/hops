2b4e55a5c5f664328c8ae0dc5e349eb9f790d159
Bram Leenders <bcleenders@gmail.com>
2016-04-08 12:16:36 +0200

    HDFS-5454 DataNode UUID should be assigned prior to FsDataset initialization

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
@@ -770,8 +770,6 @@ public class DataNode extends Configured
       storageInfo = new StorageInfo(nsInfo);
     }
 
-    checkDatanodeUuid();
-
     DatanodeID dnId =
         new DatanodeID(streamingAddr.getAddress().getHostAddress(), hostName,
             storage.getDatanodeUuid(), getXferPort(), getInfoPort(), getIpcPort());
